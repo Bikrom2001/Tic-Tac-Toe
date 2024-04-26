32e2f452253bcecc8c5f4ef5e5525c2dc9be8fa1
Bikrom-Roy <bikromroy2001@gmail.com>
2024-04-26 11:28:35 +0600

    handClick add on squares items show ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
     <button
-      onClick={handleClick}
+      onClick={onSquareClick}
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
     >
       {value}
@@ -18,24 +12,32 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState([Array(9).fill(null)]);
+
+  function handClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
 
       <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
