cf646201706da1d2f142bd38a7d848b2e340b38b
Bikrom-Roy <bikromroy2001@gmail.com>
2024-04-26 11:37:18 +0600

    all square print x values

--- src/App.jsx
+++ src/App.jsx
@@ -14,30 +14,30 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState([Array(9).fill(null)]);
 
-  function handClick() {
+  function handClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = 'X';
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
 
   return (
     <>
       <div className="flex">
-        <Square value={squares[0]} onSquareClick={handClick} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handClick(2)} />
       </div>
 
       <div className="flex">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handClick(5)} />
       </div>
 
       <div className="flex">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handClick(8)} />
       </div>
     </>
   );
